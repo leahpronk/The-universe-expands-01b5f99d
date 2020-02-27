bce9f2d2350bebcbb263ca4516e9638eb1873a2c
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2020-02-27 14:16:35 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `sterrenstelsel`.`planeten` (
+  `naam` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`naam`));
+
+INSERT into planeten (`naam`) VALUES ('Leah'); 
+
+SELECT * FROM sterrenstelsel.planeten;
+
